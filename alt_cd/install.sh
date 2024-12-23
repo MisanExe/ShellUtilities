f967dfc24a0b1cce99a17e071c4593351356c562
misan <wizbrat34@gmail.com>
2024-12-22 22:33:34 -0500

    added install script. First working prototype?

---
+++ alt_cd/install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#system directories
+sys_local="/usr/local"
+sys_local_src="$sys_local/src"
+sys_local_lib="$sys_local/lib"
+sys_local_bin="$sys_local/bin"
+
+#relevant files
+bin_file="main";
+bash_file="alt_cd"
+#program library directory name
+prog_dir="alt_cd"
+
+
+#create folder in /usr/local/lib/alt_cd
+if [ -d $sys_local_lib/$prog_dir ]; then
+	echo "Directory already exits"
+else
+	mkdir $sys_local_lib/$prog_dir
+
+	if [ $? -eq 0 ]; then 
+		echo "created $src_dir"
+	else
+		echo "failed to create $sys_local_lib/prog_dir"
+		exit 1
+	fi
+fi
+
+
+#copy main.o from build/ to /usr/local/lib/alt_cd
+cp build/main $sys_local_lib/$prog_dir/
+
+if [ -f $sys_local_lib/$prog_dir/$bin_file -a $? -eq 0 ]; then 
+	echo "added $bin_file to $sys_local/prog_dir"
+else
+	echo "failed to add $bin_file to $sys_local/$prog_dir"
+	exit 1
+fi
+
+
+#copy alt_cd from ./ to /usr/local/bin
+cp ./$bash_file $sys_local_bin/
+
+if [ -f $sys_local_bin/$bash_file -a $? -eq 0 ]; then
+	echo "added $bash_file to $sys_local_bin"
+else
+	echo "failed to add $bash_file to $bin_dir"
+	exit 1
+fi
+
+
+#add program to path
+export PATH=$PATH:/$sys_local_bin/
+
+if [ $? -eq 0 ]; then 
+	echo "installation successful !!!"
+	source ~/.bashrc
+else
+	echo "installation failed!"
+fi
+
+
--- alt_cd/main.cpp
+++ alt_cd/main.cpp
@@ -1,9 +1,7 @@
 
 #include <iostream>
-#include <vector>
 #include <string>
-#include <utility>
-#include <cstdlib>
+#include <sstream>
 
 int main(int argc, char **argv)
 {
@@ -14,35 +12,37 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	//create a vector of int and string that stores value and index
-	std::vector<std::pair<int, std::string>> args;
-	std::string arg2[argc-1];
+	//create container to hold args(files/directories)
+	std::string arg2[argc+1];
+	int index = -6; 
 
-	int index = std::strtol(argv[1], nullptr, 10); //contain the index
-	std::cout<<"chosen index : "<<index<<std::endl;
+
+	//convert the first arg in index to int
 	try
 	{
-		for(int i = 1; i<argc; i++)
+		std::stringstream ss(argv[1]);
+		ss >> index;
+	}catch(...)
+	{
+		std::cout<<"error: conversion\n";
+		exit(1);
+	}
+
+
+	try
+	{
+		int j = 0;
+		for(int i = 2; i<argc; i++)
 		{
-			args.push_back({i, argv[i]});
-			arg2[i] = argv[i];
+			arg2[j].assign( argv[i]);
+			j++;
 		}
 		
 	}catch(...){
 		std::cout<<"error occurred\n";
 	}
 
-	//print content of args
-	//for(const auto &[num, str] : args)
-	//{
-	//	std::cout<<"index : "<<num<<":  "<<str<<std::endl;
-	//}
-
 	std::cout<<arg2[index];
-
-
-
-
 
  return 0;
 }
